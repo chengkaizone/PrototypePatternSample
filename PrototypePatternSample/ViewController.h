2af74662885c2c41b7ee621424fe3232362f0f7d
chengkaizone <chengkaizone@163.com>
2015-10-10 09:08:03 +0800

    Initial Commit

---
+++ PrototypePatternSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrototypePatternSample
+//
+//  Created by lance on 15/10/10.
+//  Copyright © 2015年 lance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
